93307fa459a4e23c54702aea7425ac152c2c1417
khabib <habib.sharopov2001@mail.ru>
2023-02-20 14:43:09 +0500

    message

--- src/services/issueService.js
+++ src/services/issueService.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const issueApi = createApi({
   reducerPath: "issueApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://dry-wave-94859.herokuapp.com/",
+    baseUrl: "https://arcane-reaches-09678.herokuapp.com/",
   }),
   endpoints: (builder) => ({
     getIssues: builder.query({
--- src/services/projectService.js
+++ src/services/projectService.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const projectsApi = createApi({
   reducerPath: "projectsApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://dry-wave-94859.herokuapp.com/",
+    baseUrl: "https://arcane-reaches-09678.herokuapp.com/",
   }),
   endpoints: (builder) => ({
     getProjects: builder.query({
