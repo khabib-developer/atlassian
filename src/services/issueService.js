2cc4eb78ac87492ce098af547958720621b644fd
khabib <habib.sharopov2001@mail.ru>
2023-02-20 15:18:47 +0500

    message

--- src/services/issueService.js
+++ src/services/issueService.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const issueApi = createApi({
   reducerPath: "issueApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://arcane-reaches-09678.herokuapp.com/",
+    baseUrl: "https://relaxed-wing.80-80-218-230.plesk.page/",
   }),
   endpoints: (builder) => ({
     getIssues: builder.query({
--- src/services/projectService.js
+++ src/services/projectService.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const projectsApi = createApi({
   reducerPath: "projectsApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://arcane-reaches-09678.herokuapp.com/",
+    baseUrl: "https://relaxed-wing.80-80-218-230.plesk.page/",
   }),
   endpoints: (builder) => ({
     getProjects: builder.query({
